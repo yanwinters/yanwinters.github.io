e7153d73ac8f9cebb849cdb779efc30f4224499c
yanwinters <71384638+yanwinters@users.noreply.github.com>
2021-05-12 01:22:38 +0300

    Add files via upload

---
+++ slider3.js
@@ -0,0 +1,13 @@
+var slideshow3=new TINY.slider.slide('slideshow3', {
+  id: 'slider3',
+  auto: 6,
+  resume: false,
+  vertical: false,
+  navid: 'pagination3',
+  activeclass: 'current3',
+  position: 0,
+  rewind: false,
+  elastic: true,
+  left: 'slideleft3',
+  right: 'slideright3',
+});
---
+++ slider5.js
@@ -0,0 +1,13 @@
+var slideshow5=new TINY.slider.slide('slideshow5', {
+  id: 'slider5',
+  auto: 6,
+  resume: false,
+  vertical: false,
+  navid: 'pagination5',
+  activeclass: 'current5',
+  position: 0,
+  rewind: false,
+  elastic: true,
+  left: 'slideleft5',
+  right: 'slideright5',
+});
